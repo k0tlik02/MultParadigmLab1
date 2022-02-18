465055b90ffff9c9713ed0a485cedc13f0cafdee
Kotlyar Oleksandr <kotlyar7733@gmail.com>
2022-02-18 23:04:00 +0200

    first commit

---
+++ task1.lang_with_go_to.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <cstring>
+#include <ctype.h>
+using namespace std;
+
+
+
+int main()
+{
+
+    ifstream fin("paradigm.txt");
+
+    int n;
+    cin >> n;
+    cout << "n=" << n << endl;
+
+    vector<string> stop_words = { "for", "of", "the", "of", "on", "in", "a", "an" };
+    vector<pair<string, int>> words(100000);
+
+
+    int size = 0;
+    string word;
+    char c;
+
+
+read_word:
+    fin >> word;
+    if (isupper(word[0]))
+    {
+        word[0] += 32;
+    }
+
+
+    int i = 0;
+    bool b = false;
+search_stop_word:
+    if (i < stop_words.size()) {
+        if (word == stop_words[i]) {
+            goto read_word;
+        }
+        i++;
+        goto search_stop_word;
+    }
+
+
+
+    i = 0;
+    b = false;
+findLabel:
+    if (i < size) {
+        if (word == words[i].first) {
+            b = true;
+            words[i].second++;
+            i = size;
+        }
+        i++;
+        goto findLabel;
+    }
+
+    if (!b) {
+        words[size++] = make_pair(word, 1);
+    }
+    if (!fin.eof()) {
+        goto read_word;
+    }
+
+
+    int j = 0;
+
+    i = 0;
+Loop1:
+    if (i < size - 1) {
+        j = 0;
+    Loop2:
+        if (j < size - i - 1)
+        {
+            if (words[j].second < words[j + 1].second)
+            {
+                swap(words[j], words[j + 1]);
+            }
+
+            j++;
+            goto Loop2;
+        }
+        i++;
+        goto Loop1;
+    }
+
+
+
+
+    int index = 0;
+writting:
+    if (index < n) {
+        cout << "word: " << words[index].first << " - " << words[index].second << " times" << endl;
+        index++;
+        goto writting;
+    }
+}
---
+++ task2.lang_with_go_to.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+
+using namespace std;
+
+
+int main()
+{
+    ifstream fin("task1.txt");
+
+
+    vector<string> stop_words = { "for", "of", "the", "of", "on", "in", "a", "an" };
+    vector<pair<string, vector<int>>> alphafit(1000000, make_pair("", vector<int>(101, false)));
+    int size = 0;
+    int curLine = 0;
+    string s, line;
+    char c;
+
+
+
+readLine:
+    if (!fin.eof()) {
+        getline(fin, line);
+        curLine++;
+        stringstream ss(line);
+    readLabel:
+        if (ss >> s) {
+            int i = 0;
+            bool found = false;
+        findLabel:
+            if (i < size) {
+                if (s == alphafit[i].first) {
+                    found = true;
+                    int j = 0;
+                occurenciesLoop:
+                    if (alphafit[i].second[j] == 0) {
+                        alphafit[i].second[j] = curLine / 45 + 1;
+                        j = 100;
+                    }
+                    j++;
+                    if (j <= 100) {
+                        goto occurenciesLoop;
+                    }
+
+
+
+
+                    i = size; 
+                }
+                i++;
+                goto findLabel;
+            }
+
+            if (!found) {
+                alphafit[size].first = s;
+                alphafit[size].second[0] = curLine / 45 + 1;
+                size++;
+            }
+            goto readLabel;
+        }
+        goto readLine;
+    }
+
+
+    int j = 0;
+    int i = 0;
+    Loop1:
+    if (i < size - 1) 
+    {
+        j = 0;
+    Loop2:
+        if (j < size - i - 1) 
+        {
+            if (alphafit[j].first > alphafit[j + 1].first)
+            {
+                swap(alphafit[j], alphafit[j + 1]);
+            }
+
+            j++;
+            goto Loop2;
+        }
+        i++;
+        goto Loop1;
+    }
+
+
+    int n = size;
+    i = 0;
+    print:
+    if (i < n) {
+        if (alphafit[i].second[100] == 0) {
+            cout << alphafit[i].first << '\n';
+            int j = 0;
+        pr:
+            if (alphafit[i].second[j] != 0) {
+                cout << alphafit[i].second[j] << ' ';
+                j++;
+                goto pr;
+            }
+            cout << endl;
+        }
+        i++;
+
+        goto print;
+    }
+    return 0;
+}
